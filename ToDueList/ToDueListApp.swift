2372174dd4d00a231183905441b6d776da4911b9
Matthew Low <matthew7419@gmail.com>
2024-01-15 09:53:12 -0500

    Initial Commit

---
+++ ToDueList/ToDueListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDueListApp.swift
+//  ToDueList
+//
+//  Created by Matthew Low on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct ToDueListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
